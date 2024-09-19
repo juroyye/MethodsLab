cb26bea7d00c808ee4e853b0407489a03f9cc70a
Manning <justin.a.manning@accenture.com>
2024-09-19 10:27:27 -0500

    Added methods and completed program

---
+++ src/com/justin/DemoClass.java
@@ -0,0 +1,11 @@
+package com.justin;
+
+public class DemoClass {
+
+	  // create a generics method
+	  public <T> void genericsMethod(T data) {
+	    System.out.println("Generics Method:");
+	    System.out.println("Data Passed: " + data);
+	  }
+	}
+
